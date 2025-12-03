8f99eed5c963c58c322fae7eca1c5298f7087580
Zlata Skotnyk <skotnykzlata@gmail.com>
2025-12-03 00:21:15 -0500

    fixed applyDiscount that it can calculate discount from price*quantity

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 class ProducrProperties {
-    constructor(name, price, quantity, discount = 0){
+    constructor(name, price, quantity, discount = 0) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.discount = discount;
     }
-    getTotalValue(){
+    getTotalValue() {
         return this.price * this.quantity;
     }
-    toString(){
+    toString() {
         const discounted = ProducrProperties.applyDiscount(this.price, this.discount);
-        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}, Price after discount: $${discounted.toFixed(2)}`;
+        const totalAfterDiscount = discounted * this.quantity;
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}, Total after discount: $${totalAfterDiscount.toFixed(2)}`;
     }
 
-    static applyDiscount(price, discount){
-        let discounted = price - (price * discount);
-        return discounted;
+    static applyDiscount(price, discount) {
+        const d = (typeof discount === 'number' && !Number.isNaN(discount)) ? discount : 0;
+        const clamped = Math.min(Math.max(d, 0), 1);
+        return price * (1 - clamped);
     }
 }
 
 class PerishableProductProperties extends ProducrProperties {
-    constructor(name, price, quantity, expirationDate, discount = 0){
+    constructor(name, price, quantity, expirationDate, discount = 0) {
         super(name, price, quantity, discount);
         this.expirationDate = expirationDate;
     }
 
-    toString(){
+    toString() {
         return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
     }
 }
 
-let apple = new PerishableProductProperties("Apple", 1.5, 1, "12/12/2025", 0.1);
-let milk = new PerishableProductProperties("Milk", 10.0, 1, "17/12/2025");
+let apple = new PerishableProductProperties("Apple", 1.5, 2, "12/12/2025", 0.15);
+let milk = new PerishableProductProperties("Milk", 10.0, 1, "17/12/2025", 0.2);
 
 console.log(apple.toString());
 console.log(milk.toString());
