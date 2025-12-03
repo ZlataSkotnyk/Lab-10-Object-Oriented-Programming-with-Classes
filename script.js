6157e7828f0edfc88644c6b18725d01ed0e65d79
Zlata Skotnyk <skotnykzlata@gmail.com>
2025-12-02 22:16:02 -0500

    added two instances-apple and milk

--- script.js
+++ script.js
@@ -22,3 +22,6 @@ class PerishableProductProperties extends ProducrProperties {
         return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
     }
 }
+
+let apple = new PerishableProductProperties("Apple", 1.5, 1, "12/12/2025");
+let milk = new PerishableProductProperties("Milk", 10.0, 1, "17/12/2025");
